c1aaa2837934786ae399dd669af2b58b5728a757
何红妤#A05018 <hongyu.he@casstime.com>
2024-12-19 18:34:06 +0800

    feat: 提交cli

--- keyboard-control/dist/index.js
+++ keyboard-control/dist/index.js
@@ -1,14 +1,14 @@
-import readLine from 'node:readline'
+import readLine from 'node:readline';
 // 启用按键事件
 readLine.emitKeypressEvents(process.stdin);
 // 设置原始模式，使得输入不会被缓冲
 // 原始模式: 可以让输入流立即发送按键事件，而不是等待用户按下回车。
 // 按下control+c也不会退出终端
-process.stdin.setRawMode(true)
-process.stdin.on('keypress',(str,key)=>{
-  console.log(str,key)
-  // 按住control+c设置退出
-  if(key.sequence === '\x03'){
-    process.exit();
-  }
-})
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+    console.log(str, key);
+    // 按住control+c设置退出
+    if (key.sequence === '\x03') {
+        process.exit();
+    }
+});
