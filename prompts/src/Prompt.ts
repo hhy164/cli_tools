e800c309da16e63e2bbaaa134565f7bff8a41025
何红妤#A05018 <hongyu.he@casstime.com>
2024-12-30 23:00:05 +0800

    feat: textPrompt组件编写

--- prompts/src/Prompt.ts
+++ prompts/src/Prompt.ts
@@ -1,49 +1,58 @@
 import ansiEscapes from 'ansi-escapes';
 import EventEmitter from 'events';
-import readline from "node:readline";
+import readline from "node:readline"
 
 export interface Key {
   name: string;
   sequence: string;
 }
-
 let onKeypress: (str: string, key: Key) => void;
-
+/**
+ *  abstract定义抽象类，抽象类-不能被实例化(不可以new) 
+ *  抽象类一般作为其他类的父类，用于定义通用的行为或接口
+ */
 export abstract class Prompt extends EventEmitter {
-  value = '';
-  rl: readline.Interface = readline.createInterface({
-    input: process.stdin, // 输入流
-    output: process.stdout // 输出流
-  })
+  value = ''
+  rl: readline.Interface | undefined
   constructor() {
-    super();
-    // 监控输入
-    readline.emitKeypressEvents(process.stdin);
-    // 设置输入为原始模式
-    process.stdin.setRawMode(true);
-    // 监听键盘按下，执行函数onKeypress
-    process.stdin.on('keypress', onKeypress);
+    super()
+    /**
+     * 用于将标准输入(process.stdin)上的按键事件转化为keypress事件
+     * 默认情况下，按键输入不会触发keypress事件
+     * 与readline的逐行输入不同，这种方式可以捕获实时的按键事件，而无需等待用户按下回车
+     */
+    readline.emitKeypressEvents(process.stdin)
+    this.rl = readline.createInterface({ input: process.stdin })
+    process.stdin.setRawMode(true)
+    onKeypress = this.onKeypress.bind(this)
+    process.stdin.on('keypress', this.onKeypress)
   }
 
+  /**
+   * abstract定义抽象方法，这意味着该方法是一个没有具体实现的方法
+   * 必须由继承该抽象类的子类来实现
+   */
   abstract onKeyInput(str: string, key: Key): void;
 
-  onKeypress(str: string, key: Key) {
+  private onKeypress(str: string, key: Key) {
     if (key.sequence === '\x03') {
       process.exit()
     }
-    // 按住enter键
     if (key.name === 'return') {
       this.close()
-      return;
     }
-    this.onKeyInput(str, key)
-  }
-  close() {
-    process.stdout.write('\n');
-    process.stdin.removeListener('keypress', onKeypress)
-    process.stdin.setRawMode(false)
-    this.rl.close()
-    this.emit('submit', this.value)
+    this?.onKeyInput(str, key)
   }
 
+  public close() {
+    // 换行
+    process.stdout.write('\n')
+    // 移除监听事件
+    process.stdin.removeListener('keypress', onKeypress)
+    process.stdin.setRawMode(false)
+    // 关闭readline.Interface实例，释放输入输出流
+    this.rl?.close()
+    // 触发submit事件
+    this.emit('submit', this.value)
+  }
 }
---
+++ prompts/src/TextPrompt.ts
@@ -0,0 +1,58 @@
+import ansiEscapes from 'ansi-escapes';
+import { Key, Prompt } from './Prompt.js';
+import chalk from 'chalk';
+
+export interface TextPromptOptions {
+  type: 'text',
+  name: string,
+  message: string
+}
+// 判断是否是非打印字符
+function isNonPrintableChar(char: string) {
+  return /^[\x00-\x1F\x7F]$/.test(char);
+}
+
+export class TextPrompt extends Prompt {
+  cursor = 0;
+  constructor(private options: TextPromptOptions) {
+    super();
+  }
+  onKeyInput(str: string, key: Key): void {
+    // backspace是删除键-delete
+    if (key.name === 'backspace') {
+      this.cursor--;
+      this.value = this.value.slice(0, this.cursor)
+    }
+    if (!isNonPrintableChar(str)) {
+      this.value += str;
+      this.cursor++
+    }
+    this.render()
+  }
+  render() {
+    // 清除本行
+    process.stdout.write(ansiEscapes.eraseLine)
+    // 移动光标到当前行的开头
+    process.stdout.write(ansiEscapes.cursorTo(0))
+    process.stdout.write(`${chalk.bold(this.options.message)}
+    ${chalk.gray(',')}
+    ' '
+    ${chalk.blue(this.value)}`)
+    /**
+     * 保存当前光标位置，以便后续可以通过恢复光标位置的命令将光标移动
+     * 回到保存的位置，这是通过ANSI转义序列实现的功能，常用于终端光标控制
+     * ansiEscapes.cursorSavePosition和ansiEscapes.cursorRestorePosition配合使用
+     * 可以通过ansiEscapes.cursorRestorePosition恢复到保存的位置
+     */
+    process.stdout.write(ansiEscapes.cursorSavePosition);
+    // ansiEscapes.cursorDown(1)->光标向下移动1行
+    process.stdout.write(ansiEscapes.cursorDown(1) + ansiEscapes.cursorTo(0))
+    if (this.value === '') {
+      process.stdout.write(chalk.red('请输入名字'))
+    } else {
+      // 清空行
+      process.stdout.write(ansiEscapes.eraseLine)
+    }
+    process.stdout.write(ansiEscapes.cursorRestorePosition)
+  }
+}
