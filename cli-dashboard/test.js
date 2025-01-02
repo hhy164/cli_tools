5f39dd28adbb1a64a4c8e7ad68370df650805f61
何红妤#A05018 <hongyu.he@casstime.com>
2025-01-02 22:29:58 +0800

    feat: 引入systeminformation获取cpu相关信息

---
+++ cli-dashboard/test.js
@@ -0,0 +1,24 @@
+import si from 'systeminformation'
+
+si.currentLoad(data => {
+  // console.log(data.cpus.length)
+})
+// 获取file system 文件系统信息
+si.fsSize(data => {
+  // console.log(data)
+})
+// 获取内存总大小，已使用多少
+si.mem(data => {
+  // console.log(data)
+})
+// 获取网速
+si.networkInterfaceDefault(iface => {
+  console.log(iface)
+  si.networkStats(iface, data => {
+    console.log(data)
+  })
+})
+// 获取进程列表
+si.processes(data => {
+  console.log(data)
+})
