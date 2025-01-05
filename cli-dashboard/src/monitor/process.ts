69a98be3efe5d51abf91660faf70c34c9f04a70d
何红妤#A05018 <hongyu.he@casstime.com>
2025-01-05 21:12:19 +0800

    feat: 新增process

---
+++ cli-dashboard/src/monitor/process.ts
@@ -0,0 +1,11 @@
+import si from 'systeminformation'
+import contrib from 'blessed-contrib'
+
+const parts: Record<string, any> = {
+  p: 'pid',
+  c: 'cpu',
+  m: 'mem'
+}
+
+class ProcessMonitor {
+}
